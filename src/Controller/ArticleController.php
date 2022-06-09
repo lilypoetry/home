31a0dd4b05df71f1c54cd768773f05bae96ce7e7
Sri BLAEVOET <lilypoetry@yahoo.com>
2022-06-09 11:49:02 +0200

    Relations final

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,6 +8,7 @@ use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\AuthorRepository;
 
 class ArticleController extends AbstractController
 {
@@ -24,8 +25,16 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/article/relation', name: 'app_article_relation')]
-    public function relations(): Response
+    public function relations(AuthorRepository $authorRepository): Response
     {
-        return $this->render('article/relations.html.twig');
+        return $this->render('article/relations.html.twig', [
+            'authors' => $authorRepository->findAll()
+        ]);
     }
 }
+
+/**
+ * Pour afficher author dans l'index, simplement ajouter 
+ * 'authors' => $authorRepository->findAll() dans le tableau render(,[...])
+ * ajoute 'AuthorRepository $authorRepository' après ArticleRepository $articleRepository
+ */
